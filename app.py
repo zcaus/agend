23607510c13d5de7056d8058dc82855c90b49b82
CauaMoreira <cauamoreira2k@gmail.com>
2024-11-04 21:58:11 -0300

    att

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ try:
     pd.read_csv(arquivo_csv)
 except pd.errors.EmptyDataError:
     with open(arquivo_csv, 'w') as arquivo:
-        arquivo.write("id,data,hora,servico,nome_cliente,telefone,email,pago,cancelado\n")
+        arquivo.write("id,data,hora,servico,nome_cliente,telefone,pago,manutenção")
 
 # Função para ler agendamentos do arquivo CSV
 def ler_agendamentos():
     try:
         return pd.read_csv(arquivo_csv)
     except FileNotFoundError:
-        return pd.DataFrame(columns=['id', 'data', 'hora','servico', 'nome_cliente', 'telefone', 'email', 'pago', 'cancelado'])
+        return pd.DataFrame(columns=['id', 'data', 'hora','servico', 'nome_cliente', 'telefone', 'adiantado', 'manutenção'])
 
 # Função para salvar agendamento no arquivo CSV
 def salvar_agendamento(agendamento):
@@ -29,20 +29,16 @@ def salvar_agendamento(agendamento):
     agendamentos = agendamentos._append(agendamento, ignore_index=True)
     agendamentos.to_csv(arquivo_csv, index=False)
 
-# Função para cancelar agendamento no arquivo CSV
-def cancelar_agendamento(id_agendamento):
-    agendamentos = ler_agendamentos()
-    agendamentos.loc[agendamentos['id'] == id_agendamento, 'cancelado'] = 'Sim'
-    agendamentos.to_csv(arquivo_csv, index=False)
-
 # Interface de Usuário
-st.title("Agenda - Studio Sabrina Azeredo ")
+st.title("Agenda 💅")
 
 # Opções de Serviço
-servicos = ["Manicure Básica", "Pedicure Completo", "Banho de Gel", "Design de Unha Personalizado"]
+servicos = ["Banho de Gel", "Alongamento", "Esmaltação em Gel", "Mão Comum", "Pé Comum","Mão e Pé Comum"]
 
 # Opções de Pagamento
 pago_opcoes = ["Sim", "Não"]
+
+manu_op = ["Sim", "Não"]
 
 # Formulário de Agendamento
 with st.form("agendamento"):
@@ -52,8 +48,8 @@ with st.form("agendamento"):
     servico_escolhido = st.selectbox("Selecione o Serviço Desejado", servicos)
     nome_cliente = st.text_input("Nome do Cliente")
     telefone_cliente = st.text_input("Telefone do Cliente")
-    email_cliente = st.text_input("E-mail do Cliente")
-    pago_escolhido = st.selectbox("Já está pago?", pago_opcoes)
+    pago_escolhido = st.selectbox("Adiantamento Pago?", pago_opcoes)
+    manutencao = st.selectbox("Manutenção?", manu_op)
     submitted = st.form_submit_button("Agendar")
 
     if submitted:
@@ -63,8 +59,8 @@ with st.form("agendamento"):
            'servico': servico_escolhido,
             'nome_cliente': nome_cliente,
             'telefone': telefone_cliente,
-            'email': email_cliente,
             'pago': pago_escolhido,
+            'manutenção': manutencao,
             'cancelado': 'Não'
         }
         salvar_agendamento(agendamento)
@@ -77,11 +73,5 @@ if st.button("Consultar Agendamentos"):
     if not agendamentos.empty:
         st.write(agendamentos)
         
-        # Adicionar botão para cancelar agendamento
-        for index, row in agendamentos.iterrows():
-            if row["cancelado"] == "Não":
-                if st.button(f"Cancelar Agendamento {row['id']}"):
-                    cancelar_agendamento(row["id"])
-                    st.success(f"Agendamento {row['id']} cancelado com sucesso!")
     else:
         st.write("Nenhum agendamento realizado até o momento.")
